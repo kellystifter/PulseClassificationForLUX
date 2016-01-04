fcce51ca0ef835f52994e6581ba65ed447cafcf0
Kelly Stifter <kellystifter@users.noreply.github.com>
2016-01-03 18:56:44 -0800

    Create learningCurve.py

---
+++ learningCurve.py
@@ -0,0 +1,134 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.learning_curve import learning_curve,validation_curve
+
+def plot_learning_curve(title,clf,X,y,CV,n_jobs=1, ylim=None,train_sizes=np.linspace(.1, 1.0, 20)):
+    """
+    Generate a simple plot of the test and traning learning curve.
+
+    Parameters
+    ----------
+    clf : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+
+    cv : integer, cross-validation generator, optional
+        If an integer is passed, it is the number of folds (defaults to 3).
+        Specific cross-validation objects can be passed, see
+        sklearn.cross_validation module for the list of possible objects
+
+    n_jobs : integer, optional
+        Number of jobs to run in parallel (default 1).
+
+    train_sizes : list
+        Specify the size (as a percentage of the data set) to use
+    """
+
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+
+    train_sizes, train_scores, test_scores = learning_curve(clf, X, y, cv=CV, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+
+def plot_validation_curve(title,clf,X,y,CV,param_name,xlabel,param_range = np.logspace(-6, -1, 10),n_jobs=1, ylim=None):
+    """
+    ****Not 100% sure that this works!!!
+    Generate a simple plot of the test validation curve.
+
+    Parameters
+    ----------
+    clf : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+
+    cv : integer, cross-validation generator, optional
+        If an integer is passed, it is the number of folds (defaults to 3).
+        Specific cross-validation objects can be passed, see
+        sklearn.cross_validation module for the list of possible objects
+
+    n_jobs : integer, optional
+        Number of jobs to run in parallel (default 1).
+
+    param_name : string
+        Name of the parameter to be adjusted
+
+    xlabel : string
+        Label for the x axis of the plot
+
+    param_range : list
+        Range over which to test the specified parameter
+    """
+
+    train_scores, test_scores = validation_curve(
+        clf, X, y, param_name=param_name, param_range=param_range,
+        cv=CV, scoring="accuracy", n_jobs=n_jobs)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel("Score")
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.plot(param_range, train_scores_mean, label="Training score", color="r") #Can also choose log plot, below
+    #plt.semilogx(param_range, train_scores_mean, label="Training score", color="r")
+    plt.fill_between(param_range, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.2, color="r")
+    plt.plot(param_range, test_scores_mean, label="Cross-validation score",
+                 color="g")
+    #plt.semilogx(param_range, test_scores_mean, label="Cross-validation score",
+    #             color="g")
+    plt.fill_between(param_range, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.2, color="g")
+    plt.legend(loc="best")
+    return plt
